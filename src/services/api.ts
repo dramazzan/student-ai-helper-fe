599c54e60253782e88c583c71dda4cc5d8808480
ramazandautbek <ramazandautbek04@gmail.com>
2025-07-23 19:34:59 +0500

    fix url adsress

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.SERVER_API_URL || 'http://localhost:8080/api',
+    baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL || 'http://localhost:8080/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
